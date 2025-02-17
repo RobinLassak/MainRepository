f74f4c49e4837a32638d593015eb9680e4f60c11
RobinLassak <lassak.robin@gmail.com>
2025-02-17 12:34:36 +0100

    Add files via upload

--- Aquarium/components/Akvarium.jsx
+++ Aquarium/components/Akvarium.jsx
@@ -1,1 +1,103 @@
+import React from "react";
+import { useState } from "react";
+import "./Akvarium.css";
 
+function Akvarium(pozadovanyObjem) {
+  const [velikostAkvaria, setVelikostAkvaria] = useState({
+    sirka: 0,
+    delka: 0,
+    vyska: 0,
+  });
+  const [jeValidni, setJeValidni] = useState(false);
+
+  const handleRozmery = (e) => {
+    const zdroj = e.target.name;
+    const hodnota = e.target.value;
+    let zjistiRozmer;
+
+    switch (zdroj) {
+      case "sirka":
+        zjistiRozmer = { ...velikostAkvaria, sirka: hodnota };
+        break;
+      case "delka":
+        zjistiRozmer = { ...velikostAkvaria, delka: hodnota };
+        break;
+      case "vyska":
+        zjistiRozmer = { ...velikostAkvaria, vyska: hodnota };
+        break;
+      default:
+        break;
+    }
+    setVelikostAkvaria(zjistiRozmer);
+    validace(zjistiRozmer);
+  };
+
+  const validace = (akvarium) => {
+    let celkovyObjem =
+      Number(akvarium.sirka) * Number(akvarium.vyska) * Number(akvarium.delka);
+    console.log(akvarium);
+    console.log(celkovyObjem);
+    console.log(pozadovanyObjem);
+    if (Number(pozadovanyObjem.pozadovanyObjem) <= celkovyObjem) {
+      setJeValidni(true);
+    } else {
+      setJeValidni(false);
+    }
+  };
+
+  return (
+    <div>
+      <div className="container d-flex justify-content-center">
+        <div className="mb-3 mx-2">
+          <label htmlFor="sirka" className="label-form">
+            Sirka:
+          </label>
+          <input
+            type="number"
+            value={velikostAkvaria.sirka}
+            className="form-control"
+            name="sirka"
+            id="sirka"
+            onChange={handleRozmery}
+          />
+        </div>
+        <div className="mb-3 mx-2">
+          <label htmlFor="delka" className="label-form">
+            Delka:
+          </label>
+          <input
+            type="number"
+            value={velikostAkvaria.delka}
+            className="form-control"
+            name="delka"
+            id="delka"
+            onChange={handleRozmery}
+          />
+        </div>
+        <div className="mb-3 mx-2">
+          <label htmlFor="vyska" className="label-form">
+            Vyska:
+          </label>
+          <input
+            type="number"
+            value={velikostAkvaria.vyska}
+            className="form-control"
+            name="vyska"
+            id="vyska"
+            onChange={handleRozmery}
+          />
+        </div>
+        <div className="mb-3 mx-2 my-4">
+          <button
+            className={`btn ${jeValidni ? "btn-success" : "btn-danger"}`}
+            disabled={!jeValidni}
+          >
+            Schvalit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Akvarium;
---
+++ Aquarium/components/Rybicky.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Rybicky.css";
+
+function Rybicky({ data, smazatRybu, pridatRybu }) {
+  const [novaRyba, setNovaRyba] = useState({
+    id: data.length > 0 ? Math.max(...data.map((ryba) => ryba.id)) + 1 : 1,
+    name: "",
+    size: "",
+  });
+  const [validaceNove, setValidaceNove] = useState(false);
+
+  // Funkce pro validaci nové ryby
+  const validuj = () => {
+    if (novaRyba.name === "" || novaRyba.size === "") {
+      setValidaceNove(false);
+    } else {
+      setValidaceNove(true);
+    }
+  };
+
+  // useEffect pro spuštění validace při změně nové ryby
+  useEffect(() => {
+    validuj();
+  }, [novaRyba]);
+
+  const handleNovy = (e) => {
+    const zdroj = e.target.name;
+    const hodnota = e.target.value;
+    let pridejRybu;
+    switch (zdroj) {
+      case "name":
+        pridejRybu = { ...novaRyba, name: hodnota };
+        break;
+      case "size":
+        pridejRybu = { ...novaRyba, size: hodnota };
+        break;
+      default:
+        break;
+    }
+    setNovaRyba(pridejRybu);
+  };
+  const resetuj = () => {
+    const temp = {
+      id: novaRyba.id + 1,
+      name: "",
+      size: "",
+    };
+    setNovaRyba(temp);
+  };
+  return (
+    <div>
+      <table className="table table-success table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Jmeno</th>
+            <th scope="col">Velikost</th>
+            <th scope="col" className="text-center">
+              Smazat
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((jednaRyba) => (
+            <tr key={jednaRyba.id}>
+              <th scope="row">{jednaRyba.id}</th>
+              <td>{jednaRyba.name}</td>
+              <td>{jednaRyba.size}</td>
+              <td className="text-center">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => smazatRybu(jednaRyba.id)}
+                >
+                  X
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-4">
+            {" "}
+            <div className="mb-4">
+              <label htmlFor="name" className="form-label">
+                Jmeno:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                onChange={handleNovy}
+                value={novaRyba.name}
+              />
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="mb-3 my-4">
+              <select
+                name="size"
+                value={novaRyba.size}
+                className="form-select form-select-lg mb-3"
+                aria-label="Large select example"
+                onChange={handleNovy}
+              >
+                <option value="mala">Mala</option>
+                <option value="velka">Velka</option>
+              </select>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="my-4">
+              <button
+                type="button"
+                className="btn btn-success btn-lg"
+                disabled={!validaceNove}
+                onClick={() => {
+                  pridatRybu(novaRyba), resetuj();
+                }}
+              >
+                Pridat
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Rybicky;
